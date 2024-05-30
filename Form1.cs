c1120934cf6fdeeea7147108c2f6af9faa696826
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-30 15:12:56 -0400

    Saving progress

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,20 @@ namespace CarRentalApp
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
